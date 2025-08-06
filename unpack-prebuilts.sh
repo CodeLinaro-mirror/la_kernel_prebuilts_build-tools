97137559882467db291cd3b041267aa3878f3cae
HONG Yifan <elsk@google.com>
2025-08-06 22:46:48 +0000

    unpack-prebuilts.sh disallow 4 arguments, allow only 3.
    We have cleaned up all places where unpack-prebuilts.sh are called.
    Drop the 4-argument use case.
    
    Bug: 325514706
    Change-Id: Ie9cba1c52d03170389a4ed578423378ad873b1dc

--- unpack-prebuilts.sh
+++ unpack-prebuilts.sh
@@ -1,19 +1,13 @@
 #!/bin/bash -eu
 
-if [ "$#" -eq 4 ]; then
-  TARGET="$1"
-  LINUX="$2"
-  LINUX_MUSL="$3"
-  LINUX_MUSL_SYSROOTS="$4"
-elif [ "$#" -eq 3 ]; then
-  TARGET="$1"
-  LINUX=""
-  LINUX_MUSL="$2"
-  LINUX_MUSL_SYSROOTS="$3"
-else
-  echo "usage: $0 <target dir> [linux.zip] linux_musl.zip linux_musl_sysroots.zip" >&2
+if [ "$#" -ne 3 ]; then
+  echo "usage: $0 <target dir> linux_musl.zip linux_musl_sysroots.zip" >&2
   exit 1
 fi
+
+TARGET="$1"
+LINUX_MUSL="$2"
+LINUX_MUSL_SYSROOTS="$3"
 
 function unzip_to() {
     rm -rf "$1"
@@ -23,6 +17,5 @@ function unzip_to() {
     fi
 }
 
-unzip_to "$TARGET"/linux-x86 "$LINUX"
 unzip_to "$TARGET"/linux_musl-x86 "$LINUX_MUSL"
 unzip_to "$TARGET"/sysroots/x86_64-unknown-linux-musl "$LINUX_MUSL_SYSROOTS"
