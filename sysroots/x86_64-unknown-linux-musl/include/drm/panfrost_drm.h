de3e987c70905a34f7de714828fbfac1fc66d1fb
Ulises Mendez Martinez <umendez@google.com>
2025-12-08 14:16:29 +0000

    Update kernel-build-tools to ab/14550898
    https://ci.android.com/builds/branches/git_main-kernel-build-tools/grid?head=14550898&tail=14550898
    
    Test: treehugger
    Change-Id: I97b2c9d2a053fe5facc180d4fc858d437e9f4155

--- sysroots/x86_64-unknown-linux-musl/include/drm/panfrost_drm.h
+++ sysroots/x86_64-unknown-linux-musl/include/drm/panfrost_drm.h
@@ -19,6 +19,7 @@ extern "C" {
 #define DRM_PANFROST_PERFCNT_ENABLE 0x06
 #define DRM_PANFROST_PERFCNT_DUMP 0x07
 #define DRM_PANFROST_MADVISE 0x08
+#define DRM_PANFROST_SET_LABEL_BO 0x09
 #define DRM_IOCTL_PANFROST_SUBMIT DRM_IOW(DRM_COMMAND_BASE + DRM_PANFROST_SUBMIT, struct drm_panfrost_submit)
 #define DRM_IOCTL_PANFROST_WAIT_BO DRM_IOW(DRM_COMMAND_BASE + DRM_PANFROST_WAIT_BO, struct drm_panfrost_wait_bo)
 #define DRM_IOCTL_PANFROST_CREATE_BO DRM_IOWR(DRM_COMMAND_BASE + DRM_PANFROST_CREATE_BO, struct drm_panfrost_create_bo)
@@ -26,9 +27,11 @@ extern "C" {
 #define DRM_IOCTL_PANFROST_GET_PARAM DRM_IOWR(DRM_COMMAND_BASE + DRM_PANFROST_GET_PARAM, struct drm_panfrost_get_param)
 #define DRM_IOCTL_PANFROST_GET_BO_OFFSET DRM_IOWR(DRM_COMMAND_BASE + DRM_PANFROST_GET_BO_OFFSET, struct drm_panfrost_get_bo_offset)
 #define DRM_IOCTL_PANFROST_MADVISE DRM_IOWR(DRM_COMMAND_BASE + DRM_PANFROST_MADVISE, struct drm_panfrost_madvise)
+#define DRM_IOCTL_PANFROST_SET_LABEL_BO DRM_IOWR(DRM_COMMAND_BASE + DRM_PANFROST_SET_LABEL_BO, struct drm_panfrost_set_label_bo)
 #define DRM_IOCTL_PANFROST_PERFCNT_ENABLE DRM_IOW(DRM_COMMAND_BASE + DRM_PANFROST_PERFCNT_ENABLE, struct drm_panfrost_perfcnt_enable)
 #define DRM_IOCTL_PANFROST_PERFCNT_DUMP DRM_IOW(DRM_COMMAND_BASE + DRM_PANFROST_PERFCNT_DUMP, struct drm_panfrost_perfcnt_dump)
 #define PANFROST_JD_REQ_FS (1 << 0)
+#define PANFROST_JD_REQ_CYCLE_COUNT (1 << 1)
 struct drm_panfrost_submit {
   __u64 jc;
   __u64 in_syncs;
@@ -99,6 +102,8 @@ enum drm_panfrost_param {
   DRM_PANFROST_PARAM_NR_CORE_GROUPS,
   DRM_PANFROST_PARAM_THREAD_TLS_ALLOC,
   DRM_PANFROST_PARAM_AFBC_FEATURES,
+  DRM_PANFROST_PARAM_SYSTEM_TIMESTAMP,
+  DRM_PANFROST_PARAM_SYSTEM_TIMESTAMP_FREQUENCY,
 };
 struct drm_panfrost_get_param {
   __u32 param;
@@ -123,6 +128,11 @@ struct drm_panfrost_madvise {
   __u32 handle;
   __u32 madv;
   __u32 retained;
+};
+struct drm_panfrost_set_label_bo {
+  __u32 handle;
+  __u32 pad;
+  __u64 label;
 };
 #define PANFROSTDUMP_MAJOR 1
 #define PANFROSTDUMP_MINOR 0
